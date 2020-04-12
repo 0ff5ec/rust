71af88f5f88c7da7af85d2f2842e3cfebe04b83f
0ff5ec <offsec.akash@gmail.com>
2020-04-11 18:07:48 -0700

    added feature for play once

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -14,6 +15,15 @@ fn main() {
 
     io::stdin().read_line(&mut guess)
         .expect("Failed to read the line!");
+
+    let guess: u32 = guess.trim().parse()
+        .expect("Please enter a number!");
     
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Equal => println!("You win!"),
+        Ordering::Greater => println!("Too big!"),
+    }
 }
